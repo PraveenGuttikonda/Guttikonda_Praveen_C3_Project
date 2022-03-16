5b6e9e8ddaf0cba91a13eda0382f738edafa1aa4
PraveenGuttikonda <praveenguttikonda@gmail.com>
2022-03-16 23:53:38 +0530

    Skeleton Ordervalue code

--- src/main/java/Restaurant.java
+++ src/main/java/Restaurant.java
@@ -83,15 +83,7 @@ public class Restaurant {
 
     public int getTotalOrderValue (List <String> itemNames) {
         int orderValue =0;
-        Item item;
-        int itemIndex;
-        for (String nameOfItem :itemNames)
-        {
-            item =findItemByName(nameOfItem);
-            itemIndex= menu.indexOf(item);
-            orderValue = orderValue + menu.get(itemIndex).getPrice();
 
-        }
         return orderValue;
 
     }
